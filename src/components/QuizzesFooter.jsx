27d440b36a2cc1a75cd9c33c4181dfab6a8932a4
Demo <demolomsadze@gmail.com>
2025-05-31 23:23:25 +0400

    Adding Confetti on game won (5/5, 100 point)

--- src/components/QuizzesFooter.jsx
+++ src/components/QuizzesFooter.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./QuizzesFooter.css";
+import Confetti from "react-confetti";
 
 const QuizzesFooter = ({ quizzes, handleRestartQuiz }) => {
   // Find correct answers count from user's selected answers
@@ -18,15 +19,20 @@ const QuizzesFooter = ({ quizzes, handleRestartQuiz }) => {
   const earnedPoints = (100 / quizzes.length) * correctAnswersCount;
 
   return (
-    <footer className="quizzes-footer">
-      <h3>
-        You scored {correctAnswersCount} out of {quizzes.length} questions
-        correctly and earned {earnedPoints} points
-      </h3>
-      <button className="restart-quiz" onClick={handleRestartQuiz}>
-        Restart Quiz
-      </button>
-    </footer>
+    <>
+      <footer className="quizzes-footer">
+        <h3>
+          You scored {correctAnswersCount} out of {quizzes.length} questions
+          correctly and earned {earnedPoints} points
+        </h3>
+        <button className="restart-quiz" onClick={handleRestartQuiz}>
+          Restart Quiz
+        </button>
+      </footer>
+      {correctAnswersCount === quizzes.length && (
+        <Confetti width={window.innerWidth} height={window.innerHeight} />
+      )}
+    </>
   );
 };
 
